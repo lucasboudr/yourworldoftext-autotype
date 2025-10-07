fbccea579be8a4f2f28006594fd45db4d797334d
lucasboudr <lucas@cyberfear.com>
2025-10-06 21:15:11 -0400

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,8 @@ import pyautogui
 
 # Configuration 
 chrome_driver_path = "/path/to/chromedriver"
-use_list = True  # If True, go through all pages in words.txt
+custom_text = ""  # The text that the script will say
+use_list = True   # If True, go through all pages in words.txt
 page_name = "67"  # Page to write on (e.g. https://www.yourworldoftext.com/67)
 delay = 0.02      # Delay between each keystroke
 amount = 0        # Starting counter
@@ -31,15 +32,14 @@ if use_list:
         time.sleep(2)
         pyautogui.click(x=100, y=200)
         amount += 1
-        text = f"Hello world!\nThis message was written {amount} times\nThis message was written on {current_date}"
+        text = (custom_text)
         pyautogui.write(text, interval=delay)
 else:
     driver.get(f"https://www.yourworldoftext.com/{page_name}")
     time.sleep(2)
     pyautogui.click(x=100, y=200)
     amount += 1
-    text = f"Hello world!\nThis message was written {amount} times\nThis message was written on {current_date}"
+    text = (custom_text)
     pyautogui.write(text, interval=delay)
 
 driver.quit()
-
